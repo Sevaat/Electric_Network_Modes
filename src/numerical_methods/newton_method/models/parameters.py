40affdb5ab145b2dbb974fcc69629426225f2de3
tkachenko <sevaatmail@gmail.com>
2025-12-08 01:46:51 +0500

    Исправлено оформление класса Parameters

--- src/numerical_methods/newton_method/models/parameters.py
+++ src/numerical_methods/newton_method/models/parameters.py
@@ -1,4 +1,4 @@
-from typing import Union, Dict, Any
+from typing import Any, Dict, Union
 
 from pydantic import BaseModel, Field
 
@@ -7,9 +7,10 @@ class Parameters(BaseModel):
     """
     Класс представляющий параметры расчета
     """
-    nominal_voltage: Union[float, int] = Field(gt=0, le=1000000)            # номинальное напряжение сети
-    accuracy: Union[float, int] = Field(gt=0, le=1000000)                   # точность расчета
-    iterations: int = Field(gt=0, le=1000000)                               # количество итераций
+
+    nominal_voltage: Union[float, int] = Field(gt=0, le=1000000)  # номинальное напряжение сети
+    accuracy: Union[float, int] = Field(gt=0, le=1000000)  # точность расчета
+    iterations: int = Field(gt=0, le=1000000)  # количество итераций
 
     @classmethod
     def from_dict(cls, dict_param: Dict[str, Any]) -> Any:
@@ -18,9 +19,7 @@ class Parameters(BaseModel):
         :param dict_param: словарь входных данных параметров
         :return: экземпляр узла
         """
-        nominal_voltage = dict_param['Nominal voltage, kV']
-        accuracy = dict_param['Accuracy']
-        iterations = dict_param['Max iterations']
-        return cls(nominal_voltage=nominal_voltage,
-                   accuracy=accuracy,
-                   iterations=iterations)
+        nominal_voltage = dict_param["Nominal voltage, kV"]
+        accuracy = dict_param["Accuracy"]
+        iterations = dict_param["Max iterations"]
+        return cls(nominal_voltage=nominal_voltage, accuracy=accuracy, iterations=iterations)
