fee8c505240365b3e1fdca7ab179d2a66efbe987
tkachenko <sevaatmail@gmail.com>
2025-12-20 13:47:59 +0500

    Добавлен метод простой итерации и исправления
    исправления докстрингов для приватных методов, добавлен алгоритм, реализующий метод простой итерации, тесты.

--- src/nonlinear_systems/newton_method.py
+++ src/nonlinear_systems/newton_method.py
@@ -13,12 +13,7 @@ class NewtonMethod(ABC):
 
     @staticmethod
     def _get_power_imbalance(nodes: List[Node], conductivity_matrix: numpy.ndarray) -> List[complex]:
-        """
-        Получить небалансы мощности в узлах
-        :param nodes: список узлов
-        :param conductivity_matrix: матрица собственных и взаимных проводимостей
-        :return: небалансы мощностей по узлам
-        """
+        """Получить небалансы мощности в узлах"""
         node_count = len(nodes)
         power_imbalance: List[complex] = []
         for i in range(node_count):
@@ -52,13 +47,7 @@ class NewtonMethod(ABC):
 
     @staticmethod
     def _unbalance_condition(nodes: List[Node], parameters: Parameters, power_imbalance: List[complex]) -> bool:
-        """
-        Проверка, что все небалансы меньше заданной точности
-        :param nodes: список узлов
-        :param parameters: параметры расчета
-        :param power_imbalance: небалансы мощностей по узлам
-        :return: True - небалансы меньше заданного порога точности, False - иначе
-        """
+        """Проверка, что все небалансы меньше заданной точности"""
         for i, p_imb in enumerate(power_imbalance):
             if nodes[i].type_node != "S":
                 if abs(p_imb.real) > parameters.accuracy and abs(p_imb.imag) > parameters.accuracy:
@@ -67,14 +56,7 @@ class NewtonMethod(ABC):
 
     @staticmethod
     def _get_dpi_du(nodes: List[Node], i: int, j: int, conductivity_matrix: numpy.ndarray) -> Tuple[float, float]:
-        """
-        Получить значения производных dpi/du
-        :param nodes: список узлов
-        :param i: номер текущего узла
-        :param j: номер другого узла
-        :param conductivity_matrix: матрица собственных и взаимных проводимостей
-        :return: значения производных dpi/du
-        """
+        """Получить значения производных dpi/du"""
         if i == j:
             dpi_dui_real: List[float] = [0.0, 0.0]
             dpi_dui_imag: List[float] = [0.0, 0.0]
@@ -96,13 +78,7 @@ class NewtonMethod(ABC):
 
     @staticmethod
     def _get_row_pi(nodes: List[Node], i: int, conductivity_matrix: numpy.ndarray) -> List[float]:
-        """
-        Получить строку значений производных dpi/du
-        :param nodes: список узлов
-        :param i: номер текущего узла
-        :param conductivity_matrix: матрица собственных и взаимных проводимостей
-        :return: строка матрицы значений производных dpi/du
-        """
+        """Получить строку значений производных dpi/du"""
         row_pi_jm = []
         for j in range(len(nodes)):  # номер напряжения
             if nodes[j].type_node != "S":
@@ -113,14 +89,7 @@ class NewtonMethod(ABC):
 
     @staticmethod
     def _get_dqi_du(nodes: List[Node], i: int, j: int, conductivity_matrix: numpy.ndarray) -> Tuple[float, float]:
-        """
-        Получить значения производных dqi/du
-        :param nodes: список узлов
-        :param i: номер текущего узла
-        :param j: номер другого узла
-        :param conductivity_matrix: матрица собственных и взаимных проводимостей
-        :return: значения производных dqi/du
-        """
+        """Получить значения производных dqi/du"""
         if i == j:
             dqi_dui_real: List[float] = [0, 0]
             dqi_dui_imag: List[float] = [0, 0]
@@ -143,13 +112,7 @@ class NewtonMethod(ABC):
 
     @staticmethod
     def _get_row_qi(nodes: List[Node], i: int, conductivity_matrix: numpy.ndarray) -> List[float]:
-        """
-        Получить строку значений производных dqi/du
-        :param nodes: список узлов
-        :param i: номер текущего узла
-        :param conductivity_matrix: матрица собственных и взаимных проводимостей
-        :return: строка матрицы значений производных dqi/du
-        """
+        """Получить строку значений производных dqi/du"""
         row_qi_jm = []
         for j in range(len(nodes)):  # номер напряжения
             if nodes[j].type_node != "S":
@@ -160,12 +123,7 @@ class NewtonMethod(ABC):
 
     @staticmethod
     def _get_jacobi_matrix(nodes: List[Node], conductivity_matrix: numpy.ndarray) -> numpy.ndarray:
-        """
-        Получить матрицу Якоби
-        :param nodes: список узлов
-        :param conductivity_matrix: матрица собственных и взаимных проводимостей
-        :return: матрица Якоби
-        """
+        """Получить матрицу Якоби"""
         jacobi_matrix = []
         for i in range(len(nodes)):  # номер мощности
             if nodes[i].type_node != "S":
@@ -179,13 +137,7 @@ class NewtonMethod(ABC):
     def _get_delta_voltage(
         nodes: List[Node], power_imbalance: List[complex], jacobi_matrix: numpy.ndarray
     ) -> List[complex]:
-        """
-        Решить СЛАУ для нахождения приращений напряжений в узлах
-        :param nodes: список узлов
-        :param power_imbalance: небалансы мощностей по узлам
-        :param jacobi_matrix: матрица Якоби
-        :return: приращения напряжений в узлах
-        """
+        """Решить СЛАУ для нахождения приращений напряжений в узлах"""
         delta = []
         for i, p_imb in enumerate(power_imbalance):
             if nodes[i].type_node != "S":
@@ -196,12 +148,7 @@ class NewtonMethod(ABC):
 
     @staticmethod
     def _voltage_correction(nodes: List[Node], delta_voltage: List[complex]) -> List[Node]:
-        """
-        Скорректировать напряжения в узлах
-        :param nodes: список узлов
-        :param delta_voltage: приращения напряжений в узлах
-        :return: список узлов
-        """
+        """Скорректировать напряжения в узлах"""
         j = 0
         for i in range(len(nodes)):
             if nodes[i].type_node != "S":
@@ -211,13 +158,7 @@ class NewtonMethod(ABC):
 
     @staticmethod
     def _currents(nodes: List[Node], branches: List[Line | Transformer2 | Transformer3], conductivity_matrix: numpy.ndarray) -> List[Line | Transformer2 | Transformer3]:
-        """
-        Расчет комплексных токов в ветвях
-        :param nodes: список узлов
-        :param branches: список ветвей
-        :param conductivity_matrix: матрица собственных и взаимных проводимостей
-        :return: список ветвей
-        """
+        """Расчет комплексных токов в ветвях"""
         for branch in branches:
             if isinstance(branch, Line):
                 branch.current = (branch.start.voltage - branch.end.voltage) / branch.impedance
@@ -243,11 +184,7 @@ class NewtonMethod(ABC):
 
     @staticmethod
     def _power_losses(branches: List[Line | Transformer2 | Transformer3]) -> List[Line | Transformer2 | Transformer3]:
-        """
-        Расчет потерь мощности в элементах сети
-        :param branches: список ветвей
-        :return: список ветвей
-        """
+        """Расчет потерь мощности в элементах сети"""
         for branch in branches:
             if isinstance(branch, Line) and isinstance(branch.current, complex):
                 branch.power_losses = branch.current ** 2 * branch.impedance
@@ -280,10 +217,10 @@ class NewtonMethod(ABC):
         :return: список узлов и ветвей
         """
         conductivity_matrix = get_conductivity_matrix(nodes, branches)
-        for i in range(0, parameters.iterations):
+        for iteration in range(0, parameters.iterations):
             power_imbalance = NewtonMethod._get_power_imbalance(nodes, conductivity_matrix)
             if NewtonMethod._unbalance_condition(nodes, parameters, power_imbalance):
-                print("Точность достигнута! Расчет окончен!")
+                print(f"Точность достигнута! Расчет окончен на итерации №{iteration}!")
                 break
             jacobi_matrix = NewtonMethod._get_jacobi_matrix(nodes, conductivity_matrix)
             delta_voltage = NewtonMethod._get_delta_voltage(nodes, power_imbalance, jacobi_matrix)
